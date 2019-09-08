3ea39aea1212890399c41193baf05aad6c12a072
Peter Donald <peter@realityforge.org>
2019-09-09 09:34:57 +1000

    Add javadoc comment

--- src/main/java/org/realityforge/graphql/scalars/Scalars.java
+++ src/main/java/org/realityforge/graphql/scalars/Scalars.java
@@ -4,6 +4,9 @@ import graphql.schema.GraphQLScalarType;
 
 public final class Scalars
 {
+  /**
+   * A scalar that formats/parses a date without an offset, such as '2011-12-03'.
+   */
   public static GraphQLScalarType LOCAL_DATE =
     GraphQLScalarType.newScalar()
       .name( "LocalDate" )
